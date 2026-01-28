db6add9aff510772160ff249d6172191d08eaec7
erin-oss <erin@ripplehealth.ai>
2026-01-28 13:17:53 -0500

    hardcoding firestore

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyCgz4hEqGXXhf5qS5yMcMB8BiQrUtxlF7A",
+  authDomain: "eleven-labs-wrapper.firebaseapp.com",
+  projectId: "eleven-labs-wrapper",
+  storageBucket: "eleven-labs-wrapper.firebasestorage.app",
+  messagingSenderId: "694721678762",
+  appId: "1:694721678762:web:2000735dc9aa8f1de52fd3",
+  measurementId: "G-XKRDFNVCWV"
 };
 
 const app = initializeApp(firebaseConfig);
